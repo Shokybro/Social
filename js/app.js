1766c728a7a9af339347e4c38280c9712726862f
Shoktron <shokybro211@gmail.com>
2021-10-16 10:22:30 -0400

    Done with the posts

--- js/app.js
+++ js/app.js
@@ -9,12 +9,40 @@ postForm.addEventListener('submit', post);
 function post(e) {
     e.preventDefault();
 
+    val = postInput.value;
+
+    creatPost(val);
+
+    clearInput(postInput)
+}
+
+function creatPost (val) {
     let yost = document.createElement('div');
-    yost.classList.add('yost');
+    yost.className = "yost mb-4"
 
     let flexDiv = document.createElement('div');
     flexDiv.classList.add('flex');
 
     let profile = document.createElement('div');
-    profile.classList.add('yost--profile')
+    profile.classList.add('yost--profile');
+
+    let postTitle = document.createElement('div');
+    postTitle.classList.add('yost--title');
+    postTitle.textContent = 'Shokybro'
+    
+    let yostInfo = document.createElement('div');
+    yostInfo.classList.add('yost--info');
+    yostInfo.textContent = val;
+
+    flexDiv.appendChild(profile);
+    flexDiv.appendChild(postTitle);
+    
+    yost.appendChild(flexDiv);
+    yost.appendChild(yostInfo);
+
+    postsCollection.appendChild(yost);
+}
+
+function clearInput (input) {
+    input.value = ''
 }
